fc9dad971c00d17a138cac2aa90719bec397bf95
Malik <malikemiroglu92@gmail.com>
2023-11-09 16:42:36 +0300

    Created drawing component logic and word component logic.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ function App() {
     return word[randomIndex]
   });
 
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
 
   return (
     <div style={{
@@ -23,9 +25,11 @@ function App() {
       }}
     >
       <div style={{ fontSize: "2rem", textAlign:"center"}}>Lose Win</div>
-      <HangmanDrawing />
-      <HangmanWord />
-      <Keyboard />
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
+      <div style={{ alignSelf: "stretch"}}>
+        <Keyboard />
+      </div>
     </div>
   )
 }
